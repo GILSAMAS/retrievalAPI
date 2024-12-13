88d53d0810370ebd945aa9aadca66ceaa1d9f9bb
Manuel Gil <manuelgilsitio@gmail.com>
2024-12-12 20:09:03 -0500

    adding card operations

--- backend/src/api/card/routes.py
+++ backend/src/api/card/routes.py
@@ -16,4 +16,24 @@ async def create_card(
     session: Session = Depends(get_session),
     manager: CardManager = Depends(CardManager),
 ) -> Card:
-    return {"message": "Card created"}
+    new_card = manager.create_card(card_data, session)
+    return new_card
+
+
+@card_router.get("/get/{card_id}")
+async def get_card(
+    card_id: str,
+    session: Session = Depends(get_session),
+    manager: CardManager = Depends(CardManager),
+) -> Card:
+    card = manager.get_card(card_id, session)
+    return card
+
+@card_router.delete("/delete/{card_id}")
+async def delete_card(
+    card_id: str,
+    session: Session = Depends(get_session),
+    manager: CardManager = Depends(CardManager),
+) -> Card:
+    manager.delete_card(card_id, session)
+    return {"message": "Card deleted"}
--- backend/src/api/db/models/user.py
+++ backend/src/api/db/models/user.py
@@ -1,10 +1,10 @@
-from sqlmodel import SQLModel
-from sqlmodel import Field
+from sqlmodel import SQLModel 
+from sqlmodel import Field 
 
 from typing import Optional
-from typing import List
-from datetime import datetime
-import uuid
+from typing import List 
+from datetime import datetime 
+import uuid 
 
 from src.api.db.models.cards import Card
 from src.api.db.models.decks import Deck
@@ -12,28 +12,24 @@ from src.api.db.models.decks import Deck
 
 class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    first_name: str
-    last_name: str
+    first_name: str 
+    last_name: str 
     username: str
     email: str
     password: str
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
-    tags: Optional[List[str]] = []
-
+    
     def __repr__(self):
         return f"User {self.username}"
-
-
-class UserRecord(SQLModel, table=True):
+    
+class StudentPerformance(SQLModel, table=True):
     id: str = Field(default_factory=uuid.uuid4, primary_key=True)
-    user_id: uuid.UUID
+    user_id: uuid.UUID = Field(foreign_key="user.id")
+    
+    score: float
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
-    tags: Optional[List[str]] = []
-    decks: Optional[List[Deck]] = []
-    cards: Optional[List[Card]] = []
-    scores: Optional[List[Score]] = []
-
+    
     def __repr__(self):
-        return f"UserRecord {self.id}"
+        return f"StudentPerformance {self.id}"
