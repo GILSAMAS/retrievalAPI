140ec0f5c941c0763dcf363f421763dda76016b5
Manuel Gil <manuelgilsitio@gmail.com>
2024-12-15 11:49:44 -0500

    adding models for deck service

---
+++ backend/decks/app/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.db import init_db
+
+def lifespan(app: FastAPI):
+    print("Application startup")
+    init_db()
+    yield
+
+version = "v1"
+app = FastAPI(version=version, lifespan=lifespan)
+
+
---
+++ backend/decks/app/models/deck.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel 
+from sqlmodel import Field 
+from sqlmodel import Relationship   
+from app.models.card import Card
+import uuid 
+
+
+class Deck(SQLModel, table=True):
+    id: uuid.UUID = Field(default=uuid.uuid4(), primary_key=True)
+    name: str 
+    description: str
+    cards: list["Card"] = Relationship(back_populates="card")
+    
