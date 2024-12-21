27bd9e1f3055c4580ee2c83fa3245c77463a4aea
Manuel Gil <manuelgilsitio@gmail.com>
2024-12-21 08:08:51 -0500

    updaate

--- backend/decks/app/__init__.py
+++ backend/decks/app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import init_db
 from app.routers.decks import deck_router
 from app.routers.cards import card_router
@@ -11,5 +12,12 @@ def lifespan(app: FastAPI):
 
 version = "v1"
 app = FastAPI(version=version, lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(deck_router, prefix=f"/{version}/deck", tags=["Deck"])
 app.include_router(card_router, prefix=f"/{version}/card", tags=["Card"])
