a8bedc8b1d1f3f49cf0bc6b19dfad885cba6dcf8
Manuel Gil <manuelgilsitio@gmail.com>
2024-12-24 20:21:22 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,30 +14,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//     const url = 'http://127.0.0.1:8000/v1/deck/list';
-
-//     const {data, error, loading} = useAPI({
-//         method:'GET', 
-//         url: url, 
-//     });
-
-//     return (
-//         <>
-//         <h1>Decks</h1>
-        // {loading && <p>Loading...</p>}
-        // {error && <p>Error: {error.message}</p>}
-//         {data && (
-//             <ul>
-//             {data.map(deck => (
-//                 <li key={deck.id}>{deck.name}</li>
-//             ))}
-//             </ul>
-//         )}
-        
-//         </>
-//     );
-// }
-
-// export default App;
