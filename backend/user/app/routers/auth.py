b9faf5de828ab63d229864944079086d91c28047
Manuel Gil <manuelgilsitio@gmail.com>
2024-12-25 19:10:10 -0500

    working on auth

--- backend/user/app/routers/auth.py
+++ backend/user/app/routers/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi import status
+from fastapi import status 
 from fastapi import Depends
 from fastapi import HTTPException
 from sqlmodel import Session
@@ -7,32 +7,28 @@ from app.schemas.user import UserLogin
 from app.resources.user_manager import UserManager
 from app.db import get_session
 
-login = APIRouter()
+login_router = APIRouter()
 
-
-@login.post("/login", status_code=status.HTTP_200_OK)
-async def login_user(
-    login_data: UserLogin,
-    session: Session = Depends(get_session),
-    manager: UserManager = Depends(UserManager),
-):
+@login_router.post("/login", status_code=status.HTTP_200_OK)
+async def login_user(login_data: UserLogin, session: Session= Depends(get_session), manager: UserManager= Depends(UserManager)):
     """
     This endpoint is used to login the user
     """
     user = manager.get_user_by_email(email=login_data.email, session=session)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return {"message": "Login"}
 
-
-@login.post("/logout")
+@login_router.post("/logout")
 async def logout():
     return {"message": "Logout"}
 
-
-@login.post("/register")
-async def register():
+@login_router.post("/signup")
+async def create_user():
     return {"message": "Register"}
+
+@login_router.post("/refresh_token")
+async def get_new_access_token():
+    return {"message": "Refresh Token"}
+
+
